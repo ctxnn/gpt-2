7cfb7d43df8a9ce767e61d567ee2c3ad6d4ee374
ctxnn <taneja.chirag.ct@gmail.com>
2025-02-09 00:36:47 +0530

    add flash attention

--- train_gpt2.py
+++ train_gpt2.py
@@ -32,10 +32,12 @@ class CasualSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         # attention (materializes the large (T,T) matrix for all the queries and keys)
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        y = att @ v # (B, nh, T, T) * (B, nh, T, hs) -> (B, nh, T, hs)
+        #att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        #att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        #att = F.softmax(att, dim=-1)
+        #y = att @ v # (B, nh, T, T) * (B, nh, T, hs) -> (B, nh, T, hs)
+        # so the above 4 lines of code is simple attention but we want to use flash attention so that we can use the memory efficient version of the attention and use the meomory of our cuda device more greatly or efficiently
+        y = F.scaled_dot_product_attention(q, k, v,is_causal=True) # flash attention
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
         # output projection
         y = self.c_proj(y)
