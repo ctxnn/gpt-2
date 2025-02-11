d9cb3dea388dcb24ac79e923b06b04ecb0fb0c5f
ctxnn <taneja.chirag.ct@gmail.com>
2025-02-11 19:05:50 +0530

    add checkpoints and i think THE CODE IS FINISHED IF SOME BUG DOES NOT OCCUR LFG

--- train_gpt2.py
+++ train_gpt2.py
@@ -340,7 +340,7 @@ raw_model = model.module if ddp else model # always contains the "raw" unwrapped
 max_lr = 6e-4
 min_lr = max_lr * 0.1
 warmup_steps = 715
-max_steps = 19073
+max_steps = 19073 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
 
 def get_lr(step):
     #1 linear warmup for warmup_iters steps
@@ -389,6 +389,18 @@ for step in range(max_steps):
             print(f"validation loss: {val_loss_accum:.4f}")
             with open(log_file, "a") as f:
                 f.write(f"{step} val {val_loss_accum:.4f}\n")
+            if step > 0 and (step % 5000 == 0 or last_step):
+                # optionally write model checkpoints
+                checkpoint_path = os.path.join(log_dir, f"model_{step:05d}.pt")
+                checkpoint = {
+                    'model': raw_model.state_dict(),
+                    'config': raw_model.config,
+                    'step': step,
+                    'val_loss': val_loss_accum
+                }
+                # you might also want to add optimizer.state_dict() and
+                # rng seeds etc., if you wanted to more exactly resume training
+                torch.save(checkpoint, checkpoint_path)
         # once in a while evaluate hellaswag
     if (step % 250 == 0 or last_step) and (not use_compile):
         num_correct_norm = 0
